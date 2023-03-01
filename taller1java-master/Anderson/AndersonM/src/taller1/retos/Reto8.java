d2a7d2a05dae6fe122cfb798a9fe7cae87e6f58a
Anderson27 <anderson27mosam@gmail.com>
2023-03-01 17:54:10 +0100

    trabajo hoy

--- taller1java-master/Anderson/AndersonM/src/taller1/retos/Reto8.java
+++ taller1java-master/Anderson/AndersonM/src/taller1/retos/Reto8.java
@@ -1,5 +1,28 @@
 package taller1.retos;
-
+import java.util.*;
 public class Reto8 {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        int butget, cantidad, valor, contador, totalg, gastos, seguir, valort;
+        seguir = 1;
+        butget=100000;
+        contador = 0;
+        totalg=0;
     
+ 
+
+        for (int x= 1; x<=3; x++) {
+            System.out.println("Escriba el valor del producto ");
+            valor=lectura.nextInt();
+            System.out.println("Escriba la cantidad de productos ");
+            cantidad=lectura.nextInt();
+            contador=contador+seguir;
+           
+            valort= valor * cantidad;
+            System.out.println("ud debe pagar " + valort);
+
+
+        lectura.close();
+    }
+}
 }
